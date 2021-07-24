c21da6ec088aa59a181b834bd602ac034e56c6e9
Sheryna1 <00supergamer01@gmail.com>
2021-07-25 01:46:32 +0300

    asdad

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+
+$config = array(
+	"appcode" => "adolbus",
+	"ubi-email" => "wahan77105@britted.com",
+	"ubi-password" => "j)&Av-aGUXbDq74",
+	"default-region" => "emea", // Regions: emea - ncsa - apac 
+	"default-stats" => "casualpvp_death,casualpvp_kdratio,casualpvp_kills", // All stats: https://gist.github.com/sidelux/c2724e64acb7e1b8921c11572800f8d4
+	"default-platform" => "uplay", // Platforms: uplay - xbl - psn
+	"default-progression" => "true", // Enabled: true - false
+    'rank-images-png' => false // Rank image format: svg or png? Default: false -> svg
+);
